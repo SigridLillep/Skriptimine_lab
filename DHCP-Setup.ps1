72387eee5336fe92674f7a9b738cf3942a3b1ce4
SigridLillep <sigridlillep@gmail.com>
2025-11-14 20:32:36 +0200

    Reformat DHCP setup script for consistency

--- DHCP-Setup.ps1
+++ DHCP-Setup.ps1
@@ -12,9 +12,10 @@ Add-DhcpServerv4Scope
     -SubnetMask 255.255.255.0
 
 #Gateway seadistamine (ruuter)
-Set-ShcpServerv4OptionValue -ScopeId 10.0.22.0 -Router 10.0.22.2
+Set-DhcpServerv4OptionValue -ScopeId 10.0.22.0 -Router 10.0.22.2
 
 #DNSi seadistamine (domeenikontroller)
 Set-DhcpServerv4OptionValue -ScopeId 10.0.22.0 -DnsServer 10.0.22.10 -DnsDomain "lillep.local"
+
 
 Write-Host "DHCP server seadistatud."
